56cff885072539d448e7e5ec15c49b2b7066d97f
Mustafa Bulut <mustafaiste@outlook.com>
2025-05-20 17:37:11 +0700

    fix sitemap

--- firebase.config.ts
+++ firebase.config.ts
@@ -17,7 +17,7 @@ export const serverConfig = {
   
   export const clientConfig = {
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
